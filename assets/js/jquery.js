9ccf848d5f2935acc76e205ab71c8ee9ce6a5b9b
AranBeitia <arantza.beitia@gmail.com>
2021-08-02 12:45:23 +0200

    Events

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -1,7 +1,26 @@
-// HTML document loaded
+// Events
 let loadHTML = $('<span>HTML loaded</span>')
 $(() => loadHTML)
 
-$('#eventLoadJq').on('click', function () {
+$('#eventLoadJq').on('click', () => {
   $('#eventLoadJq').after(loadHTML)
+})
+
+$('#eventClickJq').on('click', () => {
+  let item = $('<span></span>')
+  item.text('Item clicked')
+  $('#eventClickJq').after(item)
+})
+
+$('#eventDblClickJq').on('dblclick', () => {
+  let item = $('<span>Item double clicked</span>')
+  $('#eventDblClickJq').after(item)
+})
+
+let coordenates = $('<p></p>')
+$('#eventMouseMoveJc').on('mousemove', (e) => {
+  let coorX = e.clientX
+  let coorY = e.clientY
+  coordenates.text(`Coordinates: (${coorX}, ${coorY})`)
+  $('#eventMouseMoveJc').after(coordenates)
 })
--- assets/js/vanilla.js
+++ assets/js/vanilla.js
@@ -1,10 +1,30 @@
 import { modal } from './modal.js'
 
-// HTML document loaded
+// Events
 let loadHTML = document.createElement('span')
 document.addEventListener('DOMContentLoaded' , () => loadHTML )
 
 document.getElementById('eventLoadV').addEventListener('click', (e) => {
   loadHTML.textContent = 'HTML loaded'
   e.target.parentNode.appendChild(loadHTML)
+})
+
+document.getElementById('eventClickV').addEventListener('click', (e) => {
+  let item = document.createElement('span')
+  item.textContent = 'Item clicked'
+  e.target.parentNode.appendChild(item)
+})
+
+document.getElementById('eventDblClickV').addEventListener('dblclick', (e) => {
+  let item = document.createElement('span')
+  item.textContent = 'Item double clicked'
+  e.target.parentNode.appendChild(item)
+})
+
+let coordinates = document.createElement('p')
+document.getElementById('eventMouseMoveV').addEventListener('mousemove', (e) => {
+  let coorX = e.clientX
+  let coorY = e.clientY
+  coordinates.innerHTML = `Coordinates: (${coorX}, ${coorY})`
+  e.target.parentNode.appendChild(coordinates)
 })
