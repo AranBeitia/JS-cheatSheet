3ea704bd7ffb514e5883d0c01be050ecebf89287
AranBeitia <arantza.beitia@gmail.com>
2021-08-02 18:23:14 +0200

    events

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -1,12 +1,12 @@
 // Events
-let loadHTML = $('<span>HTML loaded</span>')
+const loadHTML = $('<span>HTML loaded</span>')
 $(() => loadHTML)
 $('#eventLoadJq').on('click', () => {
   $('#eventLoadJq').after(loadHTML)
 })
 
+const item = $('<span></span>')
 $('#eventClickJq').on('click', () => {
-  let item = $('<span></span>')
   item.text('Item clicked')
   $('#eventClickJq').after(item)
 })
@@ -16,7 +16,7 @@ $('#eventDblClickJq').on('dblclick', () => {
   $('#eventDblClickJq').after(item)
 })
 
-let coordenates = $('<p></p>')
+const coordenates = $('<p></p>')
 $('#eventMouseMoveJc').on('mousemove', (e) => {
   let coorX = e.clientX
   let coorY = e.clientY
@@ -24,7 +24,7 @@ $('#eventMouseMoveJc').on('mousemove', (e) => {
   $('#eventMouseMoveJc').after(coordenates)
 })
 
-let coordOver = $('<p></p>')
+const coordOver = $('<p></p>')
 $('#eventMouseOverJc').on('mouseover', (e) => {
   let coorX = e.clientX
   let coorY = e.clientY
@@ -32,30 +32,40 @@ $('#eventMouseOverJc').on('mouseover', (e) => {
   $('#eventMouseOverJc').after(coordOver)
 })
 
-let key = $('<p></p>')
+const key = $('<p></p>')
 $(() => key)
 $(document).keydown((e) => {
   key.text(`You have pressed the key ${e.key}`)
   $('#eventKeyboardJc').after(key)
 })
 
+const submitMessage = $('<p></p>')
 $('#eventSubmitJq').on('submit', (e) => {
   e.preventDefault()
-  let message = $('<p></p>')
   let name = $('#yourNameJq').val()
-  message.text(`Hi ${name}, your form is sent correctly`)
-  $('#eventSubmitJq').after(message)
+  submitMessage.text(`Hi ${name}, your form is sent correctly`)
+  $('#eventSubmitJq').after(submitMessage)
 })
 
+const result = $('<p></p>')
 $('#eventChangeJq').change(() => {
-  let result = $('<p></p>')
   result.text(`Your animal is: ${$(event.target).val()}`)
   $('#eventChangeJq').after(result)
 })
 
+const selectMessage = $('<p></p>')
 $('#eventOpSelectJq select').change(() => {
-  let message = $('<p></p>')
   let option = $(event.target).val()
-  message.text(`Selected: ${option}`)
-  $('#eventOpSelectJq').after(message)
+  selectMessage.text(`Selected: ${option}`)
+  $('#eventOpSelectJq').after(selectMessage)
+})
+
+let checkMessage = $('<p></p>')
+$('#eventCheckJq input').on('change', () => {
+  if($(event.target).is(':checked')){
+    checkMessage.text('Is checked!!')
+  } else {
+    checkMessage.text('Not checked')
+  }
+  $('#eventCheckJq').after(checkMessage)
 })
--- assets/js/vanilla.js
+++ assets/js/vanilla.js
@@ -1,7 +1,7 @@
 import { modal } from './modal.js'
 
 // Events
-let loadHTML = document.createElement('span')
+const loadHTML = document.createElement('span')
 document.addEventListener('DOMContentLoaded' , () => loadHTML )
 
 document.getElementById('eventLoadV').addEventListener('click', (e) => {
@@ -21,7 +21,7 @@ document.getElementById('eventDblClickV').addEventListener('dblclick', (e) => {
   e.target.parentNode.appendChild(item)
 })
 
-let coordinates = document.createElement('p')
+const coordinates = document.createElement('p')
 document.getElementById('eventMouseMoveV').addEventListener('mousemove', (e) => {
   let coorX = e.clientX
   let coorY = e.clientY
@@ -29,7 +29,7 @@ document.getElementById('eventMouseMoveV').addEventListener('mousemove', (e) => 
   e.target.parentNode.appendChild(coordinates)
 })
 
-let coordOver = document.createElement('p')
+const coordOver = document.createElement('p')
 document.getElementById('eventMouseOverV').addEventListener('mouseover', (e) => {
   let coorX = e.clientX
   let coorY = e.clientY
@@ -37,31 +37,39 @@ document.getElementById('eventMouseOverV').addEventListener('mouseover', (e) => 
   e.target.parentNode.appendChild(coordOver)
 })
 
-let key = document.createElement('p')
+const key = document.createElement('p')
 document.addEventListener('DOMContentLoaded', () => key )
 document.addEventListener('keypress', (e) => {
   key.textContent = `You have pressed the key ${e.key}`
   document.getElementById('eventKeyboardV').appendChild(key)
 })
 
+const submitMessage = document.createElement('p')
 document.getElementById('eventSubmitV').addEventListener('submit', (e) => {
   e.preventDefault()
-  let message = document.createElement('p')
   let name = document.getElementById('yourName').value
-  message.textContent = `Hi ${name}, your form is sent correctly`
-  e.target.parentNode.append(message)
+  submitMessage.textContent = `Hi ${name}, your form is sent correctly`
+  e.target.parentNode.append(submitMessage)
 })
 
+const result = document.createElement('p')
 document.getElementById('eventChangeV').addEventListener('change', (e) => {
   e.preventDefault()
-  let result = document.createElement('p')
   result.textContent = `Your animal is: ${e.target.value}`
   document.getElementById('eventChangeV').parentNode.appendChild(result)
 })
 
+const selectionMessage = document.createElement('p')
 document.querySelector('#eventOpSelectV select').addEventListener('change', (e) => {
-  let message = document.createElement('p')
-  message.textContent = `Your selection is ${e.target.value}`
-  document.getElementById('eventOpSelectV').parentNode.appendChild(message)
+  selectionMessage.textContent = `Your selection is ${e.target.value}`
+  document.getElementById('eventOpSelectV').parentNode.appendChild(selectionMessage)
 })
+
+const checkMessage = document.createElement('p')
+document.getElementById('checkV').addEventListener('click', (e) => {
+  let checkbox = e.target.checked
+  checkbox ? checkMessage.textContent = 'Checked!!': checkMessage.textContent = 'Unchecked'
+  document.getElementById('eventCheckV').parentNode.appendChild(checkMessage)
+})
+
 
